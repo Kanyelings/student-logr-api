843a885556c419971688d062976bba435770eabc
Elroy Kanye <elroykanye@gmail.com>
2022-04-16 20:58:03 +0100

    implement user mapper

--- src/main/java/com/kanyelings/studentlograpi/business/mapper/LogBookMapper.java
+++ src/main/java/com/kanyelings/studentlograpi/business/mapper/LogBookMapper.java
@@ -1,6 +1,10 @@
 package com.kanyelings.studentlograpi.business.mapper;
 
 
+import com.kanyelings.studentlograpi.api.dto.LogBookDto;
+import com.kanyelings.studentlograpi.data.entity.LogBook;
+import org.mapstruct.Mapper;
+
 @Mapper(componentModel = "spring", implementationPackage="<PACKAGE_NAME>.impl")
 public interface LogBookMapper {
 
--- src/main/java/com/kanyelings/studentlograpi/business/mapper/LogrUserMapper.java
+++ src/main/java/com/kanyelings/studentlograpi/business/mapper/LogrUserMapper.java
@@ -4,12 +4,24 @@ package com.kanyelings.studentlograpi.business.mapper;
 import com.kanyelings.studentlograpi.api.dto.LogrUserDto;
 import com.kanyelings.studentlograpi.data.entity.LogrUser;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring", implementationPackage="<PACKAGE_NAME>.impl")
 public interface LogrUserMapper {
-
+	@Mappings({
+			@Mapping(source = "userId", target = "userId"),
+			@Mapping(source = "email", target = "email"),
+			@Mapping(source = "password", target = "password")
+	})
 	LogrUserDto mapLogrUserToDto(LogrUser logruser);
 
+	@Mappings({
+			@Mapping(source = "userId", target = "userId"),
+			@Mapping(source = "email", target = "email"),
+			@Mapping(source = "password", target = "password"),
+			@Mapping(target = "intern", ignore = true)
+	})
 	LogrUser mapDtoToLogrUser(LogrUserDto logruserDto);
 
 }
